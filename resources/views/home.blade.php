a84d1c75f8430dae85f7767f625c781f46a18914
sder12 <sara.cetto@gmail.com>
2022-12-16 16:10:11 +0100

    Insert database, models + controller

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,8 @@
 @section('content')
     <section>
         <h1>Questa è la homepage</h1>
+        @foreach ($movies as $movie)
+            <h2>{{$movie->title}}</h2>
+        @endforeach
     </section>
 @endsection
